a36176e3ad9ef8f1dd87f451039a094aee406add
igordmf <igordanielmf@gmail.com>
2022-03-13 23:12:38 -0300

    creating multiple buttons configurations

---
+++ src/utils.js/buttons.ts
@@ -0,0 +1,79 @@
+export const buttonsPrimary = [
+  {
+    children: 'primary',
+    color: 'primary',
+    disabled: false,
+    id: 0,
+    onClick: () => {console.log('color: primary, disable: false, icon: none')},
+  },
+  {
+    children: 'primary',
+    color: 'primary',
+    disabled: false,
+    icon: 'arrow-right',
+    id: 1,
+    onClick: () => {console.log('color: primary, disable: false, icon: arrow-right')},
+  },
+  {
+    children: 'primary',
+    color: 'primary',
+    disabled: false,
+    icon: 'check',
+    id: 2,
+    onClick: () => {console.log('color: primary, disable: false, icon: check')},
+  },
+  {
+    children: 'primary',
+    color: 'primary',
+    disabled: false,
+    icon: 'refresh',
+    id: 3,
+    onClick: () => {console.log('color: primary, disable: false, icon: refresh')},
+  },
+  {
+    children: 'primary disabled',
+    color: 'primary',
+    disabled: true,
+    id: 4,
+  }
+]
+
+export const buttonsSecondary = [
+  {
+    children: 'secondary',
+    color: 'secondary',
+    disabled: false,
+    id: 5,
+    onClick: () => {console.log('color: secondary, disable: false, icon: none')},
+  },
+  {
+    children: 'secondary',
+    color: 'secondary',
+    disabled: false,
+    icon: 'arrow-right',
+    id: 6,
+    onClick: () => {console.log('color: secondary, disable: false, icon: arrow-right')},
+  },
+  {
+    children: 'secondary',
+    color: 'secondary',
+    disabled: false,
+    icon: 'check',
+    id: 7,
+    onClick: () => {console.log('color: secondary, disable: false, icon: check')},
+  },
+  {
+    children: 'secondary',
+    color: 'secondary',
+    disabled: false,
+    icon: 'refresh',
+    id: 8,
+    onClick: () => {console.log('color: secondary, disable: false, icon: refresh')},
+  },
+  {
+    children: 'secondary disabled',
+    color: 'secondary',
+    disabled: true,
+    id: 9,
+  }
+]
